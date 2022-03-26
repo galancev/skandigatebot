c23503bd8a7da6e72527989342e5a50a6de687b4
galan <galandets@gmail.com>
2022-03-26 22:16:58 +0300

    admin sort account id desc => asc v2

--- models/user/user.go
+++ models/user/user.go
@@ -116,7 +116,7 @@ func GetUsersWithAccount(offset int, limit int) ([]models.UserAccount, error) {
 		Joins("left join tg_account on tg_account.phone = tg_user.phone").
 		Offset(offset).
 		Limit(limit).
-		Order("tg_account.id, tg_user.role_id, tg_user.active_id, tg_user.id").
+		Order("IF(tg_account.id IS NULL, 1, 0), tg_account.id, tg_user.role_id, tg_user.active_id, tg_user.id").
 		Find(&users)
 
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
